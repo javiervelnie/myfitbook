74cde1de08f5f7832aecc3537cfe12ed81c20835
javivn99 <javiervnieto99@gmail.com>
2022-03-08 13:46:16 +0100

    añadido btn añadir y menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* MOSTRAR Y OCULTAR MENU PRINCIPAL */
 const btnAbrirMenu = document.querySelector("#btn-abrir-menu");
 btnAbrirMenu.addEventListener("click", abrirMenu);
 
@@ -11,4 +12,14 @@ function abrirMenu(){
 function cerrarMenu(){
     const menuOculto = document.querySelector("#menuOculto");
     menuOculto.style.width = "0%";
+}
+
+/* MOSTRAR Y OCULTAR MENU ENTRENAMIENTOS */
+const btnAbrirMenuEntrenamientos = document.querySelector("#btn-add");
+btnAbrirMenuEntrenamientos.addEventListener("click", interaccionBoton);
+
+function interaccionBoton(){
+    const menuOculto = document.querySelector("#menuOcultoEntrenos");
+    var estaOculto = menuOculto.classList.contains( 'ocultarMenuEntrenos' );
+    (estaOculto) ? menuOculto.classList.remove( 'ocultarMenuEntrenos') : menuOculto.classList.add( 'ocultarMenuEntrenos');
 }
